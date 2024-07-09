3c944d69049501a162fe0c6584aa342b6e41f62c
Pravin Raj <128983635+pravinkr05@users.noreply.github.com>
2024-07-09 11:13:11 +0530

    Add files via upload

---
+++ Fork_Program/fork.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+int main()
+{
+       pid_t p;
+       p=fork();
+   if(p==0){
+        
+       printf("i am child having pid %d\n",getpid());
+       printf("my parents pid %d\n",getppid());
+   }
+   else{
+       printf("i am parent having pid %d\n",getpid());
+       printf("my child pid %d\n",p);
+   }
+
+    return 0;
+}
