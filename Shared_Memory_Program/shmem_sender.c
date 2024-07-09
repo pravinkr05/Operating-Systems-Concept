0127ad05a6df21bc8664899e2f6ae1b2456c9d33
Pravin Raj <128983635+pravinkr05@users.noreply.github.com>
2024-07-09 10:33:13 +0530

    Add files via upload

---
+++ Shared_Memory_Program/shmem_sender.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/shm.h>
+#include<string.h>
+#include <sys/ipc.h>
+int main(){
+char *str;
+char buff[100];
+int shmid;
+printf("\n======================================================================\n");
+shmid=shmget((key_t)1122,1024,0666|IPC_CREAT);
+printf("key of shared memory is %d \n",shmid);
+str=shmat(shmid,NULL,0);
+printf("process attached at %p\n",str);
+printf("Enter to send the year's data : \n");
+read(0,buff,100);
+strcpy(str,buff);
+printf("you wrote %s",str);
+printf("======================================================================\n\n");
+
+}
