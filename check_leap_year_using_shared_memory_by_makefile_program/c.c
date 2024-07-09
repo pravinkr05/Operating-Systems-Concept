22daf67ccd36101433d6aecfe2802e749ed771ac
Pravin Raj <128983635+pravinkr05@users.noreply.github.com>
2024-07-09 11:16:34 +0530

    Add files via upload

---
+++ check_leap_year_using_shared_memory_by_makefile_program/c.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/shm.h>
+#include<string.h>
+#include <sys/ipc.h>
+
+int main(){
+
+char *str;
+char buff[100];
+int shmid;
+printf("\n======================================================================\n");
+
+shmid=shmget((key_t)1122,1024,0666);
+printf("key of shared memory is %d \n",shmid);
+str=shmat(shmid,NULL,0);
+printf("process attached at %p\n",str);
+printf("year's data read from shared mem is %s",str);
+int a1=atoi(str);
+if(a1%4 == 0){
+printf("this is leap year \n"); 
+}
+else{
+printf("this is not leap year\n");
+}
+printf("======================================================================\n\n");
+
+}
+
